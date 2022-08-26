57c9c72c590c08c5d77dffa1c56b3c0a931515a7
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-08-26 09:16:03 -0300

    Project setup

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Pedro Boga on 26/08/22.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
