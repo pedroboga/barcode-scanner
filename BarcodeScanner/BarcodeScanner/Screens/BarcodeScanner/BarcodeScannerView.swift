3c5f65e9868c6f9a40c7f6773d9f1c57bbc12d37
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-08-28 16:57:00 -0300

    Project refactor

--- BarcodeScanner/BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
@@ -7,40 +7,27 @@
 
 import SwiftUI
 
-struct AlertItem : Identifiable {
-    let id = UUID()
-    let title: String
-    let message: String
-    let dismissButton: Alert.Button
-}
-
-struct AlertContext {
-    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input", message: "Something is wrong with the camera.", dismissButton: .default(Text("OK")))
-    static let invalidScannedType = AlertItem(title: "Invalid Scan Type", message: "The value is not valid.", dismissButton: .default(Text("OK")))
-}
-
 struct BarcodeScannerView: View {
-    @State private var scannedCode: String = ""
-    @State private var alertItem: AlertItem?
+    @StateObject var viewModel = BarcodeScannerViewModel()
     
     var body: some View {
         NavigationView {
             VStack {
-                ScannerView(scannedCode: $scannedCode, alertItem: $alertItem)
+                ScannerView(scannedCode: $viewModel.scannedCode, alertItem: $viewModel.alertItem)
                     .frame(maxWidth: .infinity, maxHeight: 300)
                 Spacer()
                     .frame(height: 60)
                 Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
                 .font(.title2)
-                Text(scannedCode.isEmpty ? "Noy Yet Scanned" : scannedCode)
+                Text(viewModel.statusText)
                     .font(.largeTitle)
                     .bold()
-                    .foregroundColor(scannedCode.isEmpty ? .red : .green)
+                    .foregroundColor(viewModel.statusTextColor)
                     .padding()
 
             }
             .navigationTitle("Barcode Scanner")
-            .alert(item: $alertItem) { alertItem in
+            .alert(item: $viewModel.alertItem) { alertItem in
                 Alert(title: Text(alertItem.title), message: Text(alertItem.message), dismissButton: alertItem.dismissButton)
             }
         }
